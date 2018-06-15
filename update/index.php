4bcc2972eaae5a63d9e3054039aba29e43758295
jpaardekooper <p_jazzzp@live.nl>
2018-06-15 16:31:59 +0200

    fixed spelling fouten

--- update/index.php
+++ update/index.php
@@ -40,7 +40,7 @@ if (isset($_SESSION["id"])) {
 
             echo " <div class=\"card mb-3\">
                 <div class=\"card-header\">
-                    <i class=\"fa fa-table\"></i> Data Table Example</div>
+                    <i class=\"fa fa-table\"></i></div>
                 <div class=\"card-body\">
                     <div class=\"table-responsive\">
                         <table class=\"table table-bordered\" id=\"table_id\" width=\"100%\" cellspacing=\"0\">                                                   
