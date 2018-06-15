43dd414f3ba8537cf5b734063d6aa4fd17caca59
jpaardekooper <p_jazzzp@live.nl>
2018-06-15 13:11:35 +0200

    I ME jasper created a new database

--- sql/software_update 5.0.sql
+++ sql/software_update 5.0.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `software_update` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `software_update`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: useragreement
+-- Host: localhost    Database: software_update
 -- ------------------------------------------------------
 -- Server version	5.7.20-log
 
@@ -43,7 +45,7 @@ CREATE TABLE `company` (
 
 LOCK TABLES `company` WRITE;
 /*!40000 ALTER TABLE `company` DISABLE KEYS */;
-INSERT INTO `company` VALUES (1,'123456789','Company','company@example.com','KVK1011101','2255QQ','Zoeterlaan 90','Zoetermeer','2018-06-06'),(2,'0701658442','DavidMon Go','jasophanger@hotmail.com','G10101ka1','6622AA','DavidLaan 22','Zoetermeer','2018-06-06');
+INSERT INTO `company` VALUES (1,'0252838392','Sqits','sqits@example.com','7348287261','2181DA','van Tetsstraat 11','Hillegom','2018-06-15'),(2,'0252517039','Metro','metro@example.com','2828491912','2184MB','Weerensteinstraat 12','Lisse','2018-06-15');
 /*!40000 ALTER TABLE `company` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,14 +59,14 @@ DROP TABLE IF EXISTS `form`;
 CREATE TABLE `form` (
   `form_id` int(11) NOT NULL AUTO_INCREMENT,
   `terms_id` int(11) DEFAULT NULL,
-  `type` enum('bug-fix','mayor-update') NOT NULL,
+  `type` enum('bug-fix','major-update') NOT NULL,
   `task_nr` varchar(45) NOT NULL,
   `version` varchar(10) NOT NULL,
   `description` text NOT NULL,
   `created_date` date NOT NULL,
   `modified_date` date NOT NULL,
   PRIMARY KEY (`form_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +75,7 @@ CREATE TABLE `form` (
 
 LOCK TABLES `form` WRITE;
 /*!40000 ALTER TABLE `form` DISABLE KEYS */;
-INSERT INTO `form` VALUES (1,1,'mayor-update','123456','1','De update heeft als volgt:\r\n\r\n1 flanfdjasfjdksa\r\n2. fjdklafjdkasfjdl\r\n3. fjkdlajfdjfkdl','2018-06-06','2018-06-06');
+INSERT INTO `form` VALUES (1,1,'major-update','0001','0.1','Verschillende functies binnen de applicatie hebben een update gekregen, waaronder:\r\n\r\n1. Login\r\n- De gebruiken kan nu inloggen zonder problemen en krijgt een sessie aangeboden via onze server.\r\n- De gebruiker kan niet op een webpagina waar een sessie voor nodig is.\r\n\r\n2. Toevoegen van gebruikers\r\n- Administratoren kunnen nu gebruikers toevoegen zonder problemen\r\n- Telefoonnummers kunnen geen \'duplicate\' errors meer geven.\r\n\r\n3. Toevoegen van formulieren en updates\r\n- Administratoren hebben nu de mogelijkheid om te kiezen tussen een \'bug fix\' en een \'major update\'. Dit maakt de update makkelijker om te sorteren en om te zien of het belangrijk is of niet.\r\n- Updates maken nu gebruik van voorwaarden en updates. Voorwaarden kunnen apart aangemaakt worden buiten update om.\r\n\r\n4. Overzicht van updates\r\n- De icoontjes voor de status van formulieren zijn aangepast om de gebruiker een goed overzicht te geven van de updates.\r\n- De gebruiker kan sorteren op verschillende waardes van updates (status, datum, etc.)\r\n- Meer informatie over de updates valt te zien voor de gebruiker.','2018-06-15','2018-06-15'),(2,1,'bug-fix','0002','0.1.2','Verschillende bugs zijn uit het systeem gehaald. De web-applicatie heeft nu goeie performance en de servers hebben geen down-time meer. We hebben verschillende bugs uit het systeem gehaald, namelijk:\r\n\r\n1. Sessie koppeling met de server\r\n- De localhost server had soms problemen met het achterhalen van de sessie van de gebruiker.\r\n- Via de PHP functies kreeg de server een verkeerde \'input\' van de localhost.\r\n\r\n2. Double the loading speed\r\n- De dubbele laad-icoontjes bij het toevoegen van formulieren en voorwaarden zijn weggehaald. Jammer genoeg moet je nu 2x zo lang wachten totdat ze zijn toegevoegd.\r\n\r\n3. Telefoonnummers\r\n- De telefoonnummers in de database werden soms op verkeerde plekken gezet. In plaats van deze prachtige RNG manier van telefoonnummers toevoegen, hebben we de kans nu opgeschroeft naar 100% voor het toevoegen van telefoonnummers aan gebruikers.\r\n\r\n4. Rare updates\r\n- We hebben de werknemers ontslagen die \"Lorem Ipsum Generator\" gebruikten voor het toevoegen van updates, rare snuiters.\r\n\r\n5. Marnix zijn microfoon probleem is opgelost\r\n- We hebben eindelijk het microfoon probleem van Marnix opgelost! Nooit meer doodse stiltes van die gast.','2018-06-15','2018-06-15');
 /*!40000 ALTER TABLE `form` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,7 +101,7 @@ CREATE TABLE `phone` (
 
 LOCK TABLES `phone` WRITE;
 /*!40000 ALTER TABLE `phone` DISABLE KEYS */;
-INSERT INTO `phone` VALUES (2,'0612345655'),(1,'0612345678');
+INSERT INTO `phone` VALUES (1,'0640386844'),(2,'0683125985');
 /*!40000 ALTER TABLE `phone` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +116,7 @@ CREATE TABLE `terms` (
   `terms_id` int(11) NOT NULL AUTO_INCREMENT,
   `acceptance` text NOT NULL,
   `service_level_agreement` text NOT NULL,
-  `contact` text NOT NULL,
+  `contract` text NOT NULL,
   `signature` text NOT NULL,
   `created_date` date NOT NULL,
   PRIMARY KEY (`terms_id`)
@@ -127,7 +129,7 @@ CREATE TABLE `terms` (
 
 LOCK TABLES `terms` WRITE;
 /*!40000 ALTER TABLE `terms` DISABLE KEYS */;
-INSERT INTO `terms` VALUES (1,'ACCEPTATIEMet het ondertekenen van dit document is de opdrachtgever zich bewust van de volgendezaken:\n\n1.De opdrachtgever geeft akkoord het product in een productieomgeving te gebruiken.Schade door incorrect functioneren van de geaccepteerde applicatie is voor risico vande accepterende klant.\n2.De opdrachtgever heeft bepaald dat het beveiligingsniveau van de applicatie afdoendeis, en heeft er bewust voor gekozen al dan niet een beveiliging analyse te latenverrichten.\n3.Alleen fouten in de applicatie die er toe leiden dat de applicatie vast loopt, lijkt vast telopen, of een foutmelding tonen aan de gebruiker, worden z.s.m. opgelost (best effort)en buiten de standaard release momenten om verholpen.4.Bugs dienen altijd via onze online helpdesk te worden gerapporteerd, voorzien van eenduidelijk omschrijving. Op deze manier kunnen we u sneller helpen, en proberen wemiscommunicatie tot een minimum te beperken.5.Met ondertekening van dit document wordt tevens de opdracht waarvoor getekend is inde opdrachtbevestiging officieel afgesloten. Er zal een factuur voor de slottermijn wordenverstuurd.6.Op al het werk dat op basis van nacalculatie of begroting wordt uitgevoerd zit gééngarantie.Meer informatie over het accepteren van een oplevering is te vinden in onze AlgemeneVoorwaarden artikel 3 met de titel “Contractsduur; uitvoeringstermijnen, uitvoering en wijzigingovereenkomst”.\n','voor deze opdracht geen Service Level Agreement (SLA) afgesloten. Alle support zal op‘best-effort’ basis worden geleverd.','voor deze opdracht geen Support-contract afgesloten. Alle werkzaamheden, waarondertechnische ondersteuning, het verhelpen van storingen, of het repareren van softwarefouten,zullen apart en op basis van nacalculatie worden gefactureerd','deze overeenkomst zijn onze Algemene Voorwaarden van toepassing, zoals te vinden oponze website. Indien gewenst kunnen de Algemene Voorwaarden per e-mail wordenopgevraagd. Ondergetekende heeft de Algemene Voorwaarden gelezen en begrepen, en gaatakkoord met deze voorwaarden','2018-05-29');
+INSERT INTO `terms` VALUES (1,'Met het ondertekenen van dit document is de opdrachtgever bewust van de volgende zaken:\r\n\r\n1. De opdrachtgever geeft akkoord het product in een productieomgeving te gebruiken. Schade door incorrect functioneren van de geaccepteerde applicatie is voor risico van de accepterende klant.\r\n\r\n2. De opdrachtgever heeft bepaald dat het beveiligingsniveau van de applicatie afdoende is, en heeft er bewust voor gekozen al dan niet een beveiliging analyse te laten verrichten.\r\n\r\n3. Alleen fouten in de applicatie die er toe leiden dat de applicatie vast loopt, lijkt vast te lopen, of een foutmelding tonen aan de gebruiker, worden z.s.m. opgelost (best effort) en buiten de standaard release momenten om verholpen.\r\n\r\n4. Bugs dienen altijd via onze online helpdesk te worden gerapporteerd, voorzien van een duidelijke omschrijving. Op deze manier kunnen we u sneller helpen, en proberen we miscommunicatie tot een minimum te beperken.\r\n\r\n5. Met ondertekening van dit document wordt tevens de opdracht waarvoor getekend is in de opdrachtbevestiging officieel afgesloten. Er zal een factuur voor de slottermijn worden verstuurd.\r\n\r\n6. Op al het werk dat op basis van nacalculatie of begroting wordt uitgevoerd zit géén garantie.','Er is voor deze opdracht geen Service Level Agreement (SLA) afgesloten. Alle support zal op \'best-effort\' basis worden geleverd.','Er is voor deze opdracht geen Support-contract afgesloten. Alle werkzaamheden, waaronder technische ondersteuning, het verhelpen van storingen, of het repareren van softwarefouten, zullen apart en op basis van nacalculatie worden gefactureerd.','Op deze overeenkomst zijn onze Algemene Voorwaarden van toepassing, zoals te vinden op onze website, Indien gewenst kunnen de Algemene Voorwaarden per e-mail worden opgevraagd. ondergetekende heeft de Algemene Voorwaarden gelezen en begrepen, en gaat akkoord met deze voorwaarden.','2018-06-15');
 /*!40000 ALTER TABLE `terms` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +149,7 @@ CREATE TABLE `update` (
   `created_date` date NOT NULL,
   PRIMARY KEY (`update_id`),
   UNIQUE KEY `update_id_UNIQUE` (`update_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +158,7 @@ CREATE TABLE `update` (
 
 LOCK TABLES `update` WRITE;
 /*!40000 ALTER TABLE `update` DISABLE KEYS */;
-INSERT INTO `update` VALUES (1,2,1,'pending','2018-06-30','2018-06-06'),(2,2,1,'pending','2018-06-30','2018-06-06'),(3,2,1,'pending','2018-06-29','2018-06-06');
+INSERT INTO `update` VALUES (1,2,1,'accepted','2018-06-30','2018-06-15');
 /*!40000 ALTER TABLE `update` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -189,7 +191,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,1,'admin@example.com','$2y$10$3oiwUsSPyMWNPVgKw1rDx.P1ZTiex7KDf2pzIyRuKvRdSgXoVjH9u','John','Doe','admin','active','2018-06-06','2018-06-06'),(2,2,'david@example.com','$2y$10$GMA9AkFOwWv513tKUFojBOeZj.7VKWoEh42mfGgJZs2vsutdgnG8u','David','Bruschkeleku','user','active','2018-06-06','2018-06-06');
+INSERT INTO `user` VALUES (1,1,'admin@example.com','$2y$10$K.E6R0iot6SXWczllZ0HtOgegQBmTtdJiPgW2vCPRYtneu8RkZtnS','Jasper','Paardekoper','admin','active','2018-06-15','2018-06-15'),(2,2,'david@example.com','$2y$10$kAresM1wEivHg4DFQIW/5eWlKH4lFsOA/CSzB9a6SSVcc1vNGMCii','David','Bruschke','user','active','2018-06-15','2018-06-15');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -202,4 +204,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-06 15:36:06
+-- Dump completed on 2018-06-15 13:02:29
