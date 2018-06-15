309633e7474e1245e27093d3537cf67705a3f0e1
jpaardekooper <p_jazzzp@live.nl>
2018-06-15 16:10:39 +0200

    real logout

--- system/logout.php
+++ system/logout.php
@@ -8,8 +8,17 @@ unset($_SESSION['id']);
 
 session_destroy();
 
+//this is for matthijs only
 echo "<div style='height: 98vh; '>
                     <img style='height: 100%; width: 100%; margin: 0; padding: 0;' src='../assets/image/for-mathijs.gif'/>
             </div>";
 
 header("Refresh: 4; URL=\"../login.php\"");
+
+
+//this is for the presentation
+/*echo "<div style='margin: 0 auto; width: 200px;'>
+                    <img style='margin-top: 40vh;' src='../assets/image/logout.gif'/>
+            </div>";
+
+header("Refresh: 2; URL=\"../login.php\"");*/
